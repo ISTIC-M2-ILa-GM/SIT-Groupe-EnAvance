850835b6d0067d596ae6999c6d25630c906c31a8
redeyes361 <redeyes_361@hotmail.com>
2019-02-08 11:59:47 +0100

    Create DroneService

--- mobile/app/src/main/java/lapin/istic/com/lapin_android/services/DroneService.java
+++ mobile/app/src/main/java/lapin/istic/com/lapin_android/services/DroneService.java
@@ -1,4 +1,31 @@
 package lapin.istic.com.lapin_android.services;
 
+import java.util.List;
+
+import lapin.istic.com.lapin_android.model.DronePath;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * @author DESCHAMPS Mathieu
+ *
+ */
 public interface DroneService {
+
+    public static final String END_POINT = "waiting...";
+
+    @POST("/api/mission")
+    Call<Integer> createMission(@Body DronePath mission);
+
+    @GET("/api/mision/result/{id}")
+    Call<List<Integer>> getResultsId(@Path("id") Integer id);
+
+    @GET("/api/image/{id}")
+    Call<ResponseBody> getImage(@Path("id") Integer id);
+
+
 }

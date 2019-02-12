c9c45ba087868d7485e42348e39548aba8fc3ea8
Hugo BANNIER <gitlab@penher.bzh>
2019-02-12 17:48:16 +0100

    Ajout de fonction pour le futur

--- drone/service/drone_service.py
+++ drone/service/drone_service.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import dronekit_sitl
 
 # Import DroneKit-Python
@@ -13,5 +14,26 @@ class DroneService:
     """
 
     def __init__(self):
+        print("Connexion au drone")
         self.vehicle = connect(connection_string, wait_ready=True)
         pass
+
+    def add_mission(self, latitude, longitude):
+        """
+        Ajoute une mission à
+        :param latitude:
+        :param longitude:
+        :return:
+        """
+
+    def start_missions(self):
+        """
+        Démarre le drone et le fait executer les missions préalablement chargé
+        :return:
+        """
+
+    def return_to_base(self):
+        """
+        Stop l'execution et retourne à la base
+        :return:
+        """

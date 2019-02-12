9cd9493c410a9eb1be2094d58cf21c4f3ab8f810
fahimmerzouk <fahimovic94@gmail.com>
2019-02-12 17:56:35 +0100

    ajout service qui recupere la derniere mission

---
+++ drone/service/retreive_last_mission.py
@@ -0,0 +1,25 @@
+from scripts.common import *
+from scripts import config
+
+
+'''
+GET /api/mission/last
+    {
+        id: ...,
+
+        points: [
+
+            {index: int, x: double, y: double, z: double},
+
+            {index: int, x: double, y: double, z: double},
+
+            {index: int, x: double, y: double, z: double}
+        ]
+    }
+
+'''
+def get_last_mission():
+    hostServer = config.hostServer
+    url = hostServer + "/api/mission/last"
+    res = get_request(url)
+    return res

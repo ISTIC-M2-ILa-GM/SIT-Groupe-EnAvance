40634a7c12cd783cbcb0a88eae9357d95634a2d4
Hugo BANNIER <gitlab@penher.bzh>
2019-02-13 08:17:30 +0100

    Ajout d'une mission ok mais non testé

--- drone/service/drone_service.py
+++ drone/service/drone_service.py
@@ -2,7 +2,8 @@
 import dronekit_sitl
 
 # Import DroneKit-Python
-from dronekit import connect
+from dronekit import connect, LocationGlobal, Command
+from pymavlink import mavutil
 
 sitl = dronekit_sitl.start_default()
 connection_string = sitl.connection_string()
@@ -18,13 +19,21 @@ class DroneService:
         self.vehicle = connect(connection_string, wait_ready=True)
         pass
 
-    def add_mission(self, latitude, longitude):
+    def add_mission(self, latitude, longitude, altitude, cancel_previous_missions=False):
         """
         Ajoute une mission à
+        :param altitude:
         :param latitude:
         :param longitude:
+        :param cancel_previous_missions:
         :return:
         """
+        cmds = self.vehicle.commands
+        cmd = Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+                      0, 0, 0, 0, 0, 0,
+                      latitude, longitude, altitude)
+        cmds.add(cmd)
+        cmds.upload()
 
     def start_missions(self):
         """
@@ -35,5 +44,11 @@ class DroneService:
     def return_to_base(self):
         """
         Stop l'execution et retourne à la base
+        :return:
+        """
+
+    def get_position(self):
+        """
+
         :return:
         """

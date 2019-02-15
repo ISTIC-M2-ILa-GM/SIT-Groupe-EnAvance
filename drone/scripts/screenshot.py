017a3b317ca261f7a6165a229e25b04876dd6112
fahimmerzouk <fahimovic94@gmail.com>
2019-02-15 10:51:07 +0100

    function take_screenshot

--- drone/scripts/screenshot.py
+++ drone/scripts/screenshot.py
@@ -1,2 +1,13 @@
 import os
-os.system("import -window root temp/screenshot.png")
+
+
+def take_screenshot():
+    screen = "temp/screenshot.png"
+    command = "import -window root "+screen
+    os.system(command)
+    print(command)
+    return screen
+
+
+if __name__ == '__main__':
+    take_screenshot()

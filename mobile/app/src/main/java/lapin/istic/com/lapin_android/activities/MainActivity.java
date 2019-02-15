751e64071a0c99fe57153703e0a53b8102cd9606
NoureddineK <noureddine.kadri05@gmail.com>
2019-02-15 09:43:50 +0100

    Refactor Post activity

--- mobile/app/src/main/java/lapin/istic/com/lapin_android/activities/MainActivity.java
+++ mobile/app/src/main/java/lapin/istic/com/lapin_android/activities/MainActivity.java
@@ -1,14 +1,12 @@
 package lapin.istic.com.lapin_android.activities;
 
 import android.content.Intent;
-import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -84,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
         testnk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), TestingActivityNK.class);
+                Intent intent = new Intent(getBaseContext(), PostTestingActivity.class);
                 startActivityForResult(intent, 0);
 
             }
